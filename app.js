3593b56d435878b3f7c51ac950f24b473c7e7542
Ali Ghaleb <alighaleb2001@gmail.com>
2023-08-23 19:14:10 +0300

    post-req

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
 const express = require("express");
 
-
 //Init app
 const app = express();
 
 
+// Apply Middlewares
+app.use(express.json());
+
 const books = [
-    {
-        id:1,
-        title:"Book1",
-        author: "B1",
-        description:"About B1",
-        price: 10,
-        cover: "cover"
-    },
-    {
-        id:2,
-        title:"Book2",
-        author: "B2",
-        description:"About B2",
-        price: 12,
-        cover: "cover"
-    }
-
-
-]
-
+  {
+    id: 1,
+    title: "Book1",
+    author: "B1",
+    description: "About B1",
+    price: 10,
+    cover: "cover",
+  },
+  {
+    id: 2,
+    title: "Book2",
+    author: "B2",
+    description: "About B2",
+    price: 12,
+    cover: "cover",
+  },
+];
 
 // Http Methods / Verbs
 
+app.get("/api/books", (req, res) => {
+  res.status(200).json(books);
+});
 
-app.get("/api/books", (req,res)=>{
-    res.status(200).json(books);
-})
+app.get("/api/books/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404).json({ message: "book not found" });
+  }
+});
 
-app.get("/api/books/:id", (req,res)=>{
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if(book){
-        res.status(200).json(book);
+
+app.post("/api/books", (req,res) =>{
+    console.log(req.body);
+    const book = {
+        id: books.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        price: req.body.price,
+        cover: req.body.cover
     }
-    else{
-        res.status(404).json({message: "book not found"});
-    }
-})
 
+    books.push(book);
+    res.status(201).json(book);
+})
 
 const PORT = 5000;
 
